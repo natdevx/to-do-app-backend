dffae90fdd18f8e086b3d546343a304ee41cfca9
Nat Arrazola <NatyArrazola13@gmail.com>
2025-08-10 12:53:13 -0600

    Agregamos a cors el origen permitido de nuestro front

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: ["http://localhost:5173", "https://gleaming-entremet-b1556a.netlify.app"]
 }))
 
 app.use(express.json())
